3441f3c319b408b625baf48f2470c2becd7f927d
Zach 'Leif' Eriksen <zmeriksen@gmail.com>
2019-05-29 19:57:57 -0500

    added basic websocket sample code

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,8 +2,32 @@ import Vapor
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
+    let sessionManager = TrackingSessionManager.tracking
     
     let usersController = UserController()
     try router.register(collection: usersController)
     
+    // MARK: Status Checks
+    router.get("status") { _ in "ok \(Date())" }
+    
+    router.get("word-test") { request in
+        return wordKey(with: request)
+    }
+    
+    // MARK: Poster Routes
+    router.post("create", use: sessionManager.createTrackingSession)
+    
+    router.post("close", TrackingSession.parameter) { req -> HTTPStatus in
+        let session = try req.parameters.next(TrackingSession.self)
+        sessionManager.close(session)
+        return .ok
+    }
+    
+    router.post("update", TrackingSession.parameter) { req -> Future<HTTPStatus> in
+        let session = try req.parameters.next(TrackingSession.self)
+        return try Location.decode(from: req).map(to: HTTPStatus.self) { location in
+            sessionManager.update(for: session)
+            return .ok
+        }
+    }
 }
